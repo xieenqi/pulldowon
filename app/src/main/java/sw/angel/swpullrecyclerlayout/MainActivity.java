a9c105f5d8e9e4915ac5ecc5a0d0c5dec5695555
xieenqi <329191055@qq.com>
2017-04-08 22:12:30 +0800

    bug媳妇

--- app/src/main/java/sw/angel/swpullrecyclerlayout/MainActivity.java
+++ app/src/main/java/sw/angel/swpullrecyclerlayout/MainActivity.java
@@ -1,6 +1,7 @@
 package sw.angel.swpullrecyclerlayout;
 
 import android.app.Activity;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -8,6 +9,7 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+
 import com.angel.interfaces.OnTouchUpListener;
 import com.angel.layout.SWPullRecyclerLayout;
 
@@ -19,10 +21,12 @@ public class MainActivity extends Activity implements OnTouchUpListener {
     private SWPullRecyclerLayout recycler;
     private View header;
     private View footer;
+    private Handler handler;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        handler = new Handler();
         inital();
     }
 
@@ -32,10 +36,10 @@ public class MainActivity extends Activity implements OnTouchUpListener {
         for (int i = 0; i < 30; i++) {
             list.add(i + 1 + "");
         }
-        header= LayoutInflater.from(this).inflate(R.layout.header,null);
-        footer= LayoutInflater.from(this).inflate(R.layout.footer,null);
-        recycler.addHeaderView(header,100);
-        recycler.addFooterView(footer,100);
+        header = LayoutInflater.from(this).inflate(R.layout.header, null);
+        footer = LayoutInflater.from(this).inflate(R.layout.footer, null);
+        recycler.addHeaderView(header, 100);
+        recycler.addFooterView(footer, 100);
         NumAdapter adapter = new NumAdapter(this, list);
         recycler.setMyRecyclerView(new LinearLayoutManager(this), adapter);
         recycler.addOnTouchUpListener(this);
@@ -45,15 +49,25 @@ public class MainActivity extends Activity implements OnTouchUpListener {
     public void OnRefreshing() {
         Log.i("angel", "OnRefreshing: 正在刷新");
         //刷新完执行
-//        recycler.setIsScrollRefresh(false);
-//        recycler.setScrollTo(recycler.getTotal(),0);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                recycler.setIsScrollRefresh(false);
+                recycler.setScrollTo(recycler.getTotal(), 0);
+            }
+        }, 2000);
     }
 
     @Override
     public void OnLoading() {
         Log.i("angel", "OnLoading: 正在加载");
         //加载完执行
-//        recycler.setIsScrollLoad(false);
-//        recycler.setScrollTo(recycler.getTotal(),0);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                recycler.setIsScrollLoad(false);
+                recycler.setScrollTo(recycler.getTotal(), 0);
+            }
+        }, 2000);
     }
 }
